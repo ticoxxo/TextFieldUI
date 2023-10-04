525c1d527e146dfa6be7482a1786c0d5f5221f04
Ticoxxo <ticoxxo@gmail.com>
2023-10-04 10:44:43 -0700

    Initial Commit

---
+++ TextFieldUI/TextFieldUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldUIApp.swift
+//  TextFieldUI
+//
+//  Created by Alberto Almeida on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
